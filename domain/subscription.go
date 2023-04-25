e96b289a83946cf641cdab14b38851b3cb0448c5
AGO-90 <andres.ghione@gmail.com>
2023-04-24 21:12:50 -0300

    subscription gorm mapped

--- domain/subscription.go
+++ domain/subscription.go
@@ -1,11 +1,11 @@
 package domain
 
 type Subscription struct {
-	ID               int64     `json:"id"`
-	Name             string    `json:"name"`
-	Description      string    `json:"description"`
-	MonthlyPrice     float64   `json:"monthly_price"`
-	DurationInMonths int64     `json:"duration_in_months"`
-	Users            []User    `json:"users"`
-	Services         []Service `json:"services"`
+	ID               int64     `json:"id" gorm:"primaryKey"`
+	Name             string    `json:"name" gorm:"size:100"`
+	Description      string    `json:"description" gorm:"size:100"`
+	MonthlyPrice     float64   `json:"monthly_price" gorm:"default:1"`
+	DurationInMonths int64     `json:"duration_in_months" gorm:"default:1"`
+	Users            []User    `json:"users" gorm:"foreignKey:ID"`
+	Services         []Service `json:"services"  gorm:"foreignKey:ID"`
 }
